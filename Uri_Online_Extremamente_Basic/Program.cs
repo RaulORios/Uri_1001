8ed5065841c0eb6d2a0f86ae82399395bb770916
RaulORios <raul.orios89@outlook.com>
2021-08-10 10:44:08 -0300

    Program.cs adicionado

---
+++ Uri_Online_Extremamente_Basic/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Uri_Online_Extremamente_Basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, x;
+
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+
+            x = a + b;
+
+            Console.WriteLine($"X = {x}");
+                                             
+        }
+    }
+}
